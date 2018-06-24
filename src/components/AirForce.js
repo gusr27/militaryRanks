e48c60a95900b53508a6f52cc609b2041fb204ff
Gus Richardson <gus.richardson@icloud.com>
2018-06-24 14:39:09 -0400

    Added grid and styling to AF page

--- src/components/AirForce.js
+++ src/components/AirForce.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react'
-import { Card,CardItem, Body, Content, Icon, Text, Segment, List, ListItem, Button} from 'native-base'
-import {Image} from 'react-native'
+import { Container, Card,CardItem, Body, Content, Icon, Text,Thumbnail, Segment, List, ListItem, Button, Left, Right} from 'native-base'
+import {Image, FlatList, View} from 'react-native'
+import { Col, Row, Grid } from 'react-native-easy-grid'
 import enlistedData from './AirForceList.json'
 class AirForce extends Component {
+
+  loadChanges(){
+      
+  }
     
     
   render(){
@@ -10,33 +15,63 @@ class AirForce extends Component {
 
     
         
-      <Content hasSegment>
-        <Segment>
-          <Button first active={true}><Text>Enlisted</Text></Button>
-          <Button last ><Text>Officer</Text></Button>
-        </Segment>
-        <List dataArray={enlistedData}
-            renderRow={(enlistedData) =>
-              <ListItem>
-                <Card>
-                  <CardItem>
-                    <Body>
-                      <Text>{enlistedData.title}</Text>
-                      
-                    </Body>
-                  </CardItem>
-                    <CardItem cardBody>
-                        <Image source={{uri: enlistedData.image}} style={{height: 200, width: null, flex: 1}}/>
-                    </CardItem>
-                </Card>
-              </ListItem>
-            }>
-          </List>
-      </Content>
-
-
+     <Content>
+    
+        
+      <Grid>
+        
+            <Row>
+                <Col>
+                    <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/E2_USAF_AM.svg/1425px-E2_USAF_AM.svg.png"}} resizeMode={"contain"} style={{height: 50, width: 50}}></Image>
+                </Col> 
+                <Col>
+                     <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/E3_USAF_AM1.svg/1024px-E3_USAF_AM1.svg.png"}} resizeMode={"contain"}style={{height: 50, width: 50}}></Image>
+                </Col>
+                <Col>
+                     <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/E4_USAF_SAM.svg/1024px-E4_USAF_SAM.svg.png"}} resizeMode={"contain"} style={{height: 50, width: 50}}></Image>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/E2_USAF_AM.svg/1425px-E2_USAF_AM.svg.png"}} resizeMode={"contain"} style={{height: 50, width: 50}}></Image>
+                </Col> 
+                <Col>
+                     <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/E3_USAF_AM1.svg/1024px-E3_USAF_AM1.svg.png"}} resizeMode={"contain"}style={{height: 50, width: 50}}></Image>
+                </Col>
+                <Col>
+                     <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/E4_USAF_SAM.svg/1024px-E4_USAF_SAM.svg.png"}} resizeMode={"contain"} style={{height: 50, width: 50}}></Image>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/E2_USAF_AM.svg/1425px-E2_USAF_AM.svg.png"}} resizeMode={"contain"} style={{height: 50, width: 50}}></Image>
+                </Col> 
+                <Col>
+                     <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/E3_USAF_AM1.svg/1024px-E3_USAF_AM1.svg.png"}} resizeMode={"contain"}style={{height: 50, width: 50}}></Image>
+                </Col>
+                <Col>
+                     <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/E4_USAF_SAM.svg/1024px-E4_USAF_SAM.svg.png"}} resizeMode={"contain"} style={{height: 50, width: 50}}></Image>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/E2_USAF_AM.svg/1425px-E2_USAF_AM.svg.png"}} resizeMode={"contain"} style={{height: 50, width: 50}}></Image>
+                </Col> 
+                <Col>
+                     <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/E3_USAF_AM1.svg/1024px-E3_USAF_AM1.svg.png"}} resizeMode={"contain"}style={{height: 50, width: 50}}></Image>
+                </Col>
+                <Col>
+                     <Image source={{uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/E4_USAF_SAM.svg/1024px-E4_USAF_SAM.svg.png"}} resizeMode={"contain"} style={{height: 50, width: 50}}></Image>
+                </Col>
+            </Row>
+    </Grid>
+    </Content>
     )
   }
+}
+
+const styles={
+     
 }
 
 export default AirForce
---
+++ src/components/InfoPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Header, Body, Text, Content} from 'native-base'
+const InfoPage = (props){
+    return(
+    
+    )
+}
+
+export default InfoPage
