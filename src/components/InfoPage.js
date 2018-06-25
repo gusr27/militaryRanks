5ce316d1ef04f248681a1cda759d7ec2fd64573f
Gus Richardson <gus.richardson@icloud.com>
2018-06-25 01:29:37 -0400

    Added layout and data usage

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -1,18 +1,36 @@
 import React from 'react'
-import {Header, Body, Text, Content} from 'native-base'
+import {Header, Body, Text, Content, Card, CardItem, Left, Right, Image, Thumbnail} from 'native-base'
+
 const InfoPage = (props) => {
     
-    
+    const {rate, image, description, abv, nato, address} = props.data   
     
     return(
         <Content>
-            <Text>{props.rate}</Text>
-            <Text>{props.description}</Text>
-    
-        </Content>
-        
-        
-        
+            
+            <Card>
+                 <CardItem>
+                    <Left>
+                        <Thumbnail source={{uri: image}} resizeMode={"contain"} square/> 
+                        <Body>
+                            <Text>{rate}</Text>
+                            <Text note>Abbreviation: {abv}</Text>
+                            <Text note>NATO Code: {nato}</Text>
+                            <Text note>Addressed as: {address}</Text>
+                        </Body>
+                    </Left>
+                    
+                </CardItem>
+                <CardItem>
+                    <Body>
+                        <Text>{description}</Text>
+                    </Body>
+                </CardItem>
+                <CardItem>
+                
+                </CardItem>
+            </Card>
+        </Content> 
     )
 }
 
