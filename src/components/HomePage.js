43033f33985bb97b6600584366bbddc29e145592
Gus Richardson <gus.richardson@icloud.com>
2018-06-25 02:00:10 -0400

    Styled HomePage and added logo assets

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react'
-import {Container, Card, CardItem,Header, Title, Content, Button, Left, Right, Body, Icon, Text, Segment} from 'native-base'
+import {Container, Card, CardItem,Header, Title, Content, Button, Left, Right, Body, Icon, Text, Segment, List, ListItem, Thumbnail} from 'native-base'
 import {Actions} from 'react-native-router-flux'
 
 class HomePage extends Component{
     render(){
         return(
-           <Content>
-                <Button onPress= {() => {Actions.airforce(); }}><Text>Air Force</Text></Button>
-          <Button onPress= {() => {Actions.army(); }}><Text>Army</Text></Button>
-          <Button onPress= {() => {Actions.marines(); }}><Text>Marine Corps</Text></Button>
-          <Button onPress= {() => {Actions.navy(); }}><Text>Navy</Text></Button>
+           <Content contentContainerStyle={{flex: 1}}>
+              
+                
+                <List>
+                    <ListItem onPress= {() => {Actions.airforce(); }}>
+                        <Thumbnail square size={80} source={require("../assets/images/AirForceLogo.svg.png")} />
+                        <Body>
+                            <Text>Air Force</Text>
+                            <Text note>Tap to See Ranks</Text>
+                        </Body>
+                    </ListItem>
+                    <ListItem onPress= {() => {Actions.army(); }}>
+                        <Thumbnail square size={80} source={require("../assets/images/ArmyLogo.png")} />
+                        <Body>
+                            <Text>Army</Text>
+                            <Text note>Tap to See Ranks</Text>
+                        </Body>
+                    </ListItem >
+                    <ListItem onPress= {() => {Actions.marines(); }}>
+                        <Thumbnail square size={80} source={require("../assets/images/MarineCorpsLogo.png")} />
+                        <Body>
+                            <Text>Marine Corps</Text>
+                            <Text note>Tap to See Ranks</Text>
+                        </Body>
+                    </ListItem>
+                    <ListItem onPress= {() => {Actions.navy(); }}>
+                        <Thumbnail square size={80} source={require("../assets/images/NavyLogo.png")} />
+                        <Body>
+                            <Text>Navy</Text>
+                            <Text note>Tap to See Ranks</Text>
+                        </Body>
+                    </ListItem>
+                </List>
             </Content>
           
         )
