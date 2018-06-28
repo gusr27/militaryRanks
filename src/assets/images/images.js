8a7b11041fe6646bce5f444594ee561ea044d45b
Gus Richardson <gus.richardson@icloud.com>
2018-06-27 21:22:48 -0400

    Fixed styling

---
+++ src/assets/images/images.js
@@ -0,0 +1,87 @@
+
+
+const images = {
+    AFLogo: require('./AirForceLogo.svg.png'),
+    NavyLogo: require('./NavyLogo.png'),
+    MarineCorpsLogo: require('./MarineCorpsLogo.png'),
+    ArmyLogo: require('./ArmyLogo.png'),
+    AF: [
+        {
+            id:0,
+            ranks:[
+                require('./USAF13.png'),
+                require('./USAF12.png'),
+                require('./USAF11.png'),
+                require('./USAF10.png'),
+                require('./USAF9.png'),
+                require('./USAF8.png'),
+                require('./USAF7.png'),
+                require('./USAF6.png'),
+                require('./USAF4.png'),
+                require('./USAF3.png'),
+                require('./USAF2.png'),
+                require('./USAF1.png')
+                
+            ]
+        }
+    ],
+    Army:[
+        {
+            id:1,
+            ranks:[
+              require('./Army1.svg.png'),
+              require('./Army2.png'),
+              require('./Army3.png'),
+              require('./Army4.png'),
+              require('./Army5.png'),
+              require('./Army6.png'),
+              require('./Army7.png'),
+              require('./Army8.png'),
+              require('./Army9.png'),
+                require('./Army10.png'),
+                require('./Army11.png'),
+                require('./Army12.png')
+            ]
+        }
+    ],
+    Marines:[
+        {
+            id:2,
+            ranks:[
+              require('./USMC1.png'),
+              require('./USMC2.png'),
+              require('./USMC3.png'),
+              require('./USMC4.png'),
+              require('./USMC5.png'),
+              require('./USMC6.png'),
+              require('./USMC7.png'),
+              require('./USMC8.png'),
+              require('./USMC9.png'),
+              require('./USMC10.png'),
+              require('./USMC11.png')
+              
+            ]
+        }
+    ],
+    Navy:[
+        {
+            id:3,
+            ranks:[
+                require('./Navy12.png'),
+              require('./Navy11.png'),
+              require('./Navy10.png'),
+              require('./Navy9.png'),
+              require('./Navy8.png'),
+              require('./Navy7.png'),
+              require('./Navy6.png'),
+              require('./Navy5.png'),
+              require('./Navy4.png'),
+                require('./Navy3.png'),
+                require('./Navy2.png'),
+                require('./Navy1.png')
+            ]
+        }
+    ]
+}
+
+export default images
