0c945d0ab6b29302589cf6836b61dadc285c4d4b
Gus Richardson <gus.richardson@icloud.com>
2018-07-05 17:29:27 -0400

    Initial setup of MenuPage

---
+++ src/components/MenuPage.js
@@ -0,0 +1,35 @@
+import React, {Component}from 'react'
+import {Container, Card, CardItem,Header, Title, Content, Button, Left, Right, Body, Icon, Text, Segment, List, ListItem, Thumbnail, Drawer, Footer} from 'native-base'
+import {Actions} from 'react-native-router-flux'
+import Images from '@images/images'
+
+class MenuPage extends Component{
+   
+    
+    render(){
+        
+    
+        return(
+            
+           <Content contentContainerStyle={{flex: 1}}>
+              
+                
+                <List>
+                    <ListItem onPress= {() => {Actions.airforce(); }}>
+                        <Thumbnail square size={80} source={Images.AFLogo} />
+                        <Body>
+                            <Text>Air Force</Text>
+                            <Text note>Tap to See Ranks</Text>
+                        </Body>
+                    </ListItem>
+                    
+                </List>
+               
+            </Content>
+            
+          
+        )
+    }
+}
+
+export default MenuPage
